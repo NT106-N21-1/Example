f94f92dcdc50bf1fb369c3a7411acca3c2427a69
Bao Nguyen <nvbao45@gmail.com>
2023-03-31 08:17:44 +0700

    lab2: hoàn thành bài 1

--- lab2/Form1.cs
+++ lab2/Form1.cs
@@ -6,5 +6,23 @@ namespace lab2
 		{
 			InitializeComponent();
 		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+			openFileDialog.Filter = "Text file|*.txt";
+			if (openFileDialog.ShowDialog() == DialogResult.OK){
+				string filePath = openFileDialog.FileName;
+				
+				FileStream fs = new FileStream(filePath, FileMode.Open);
+				StreamReader sr = new StreamReader(fs);
+				int lineCount = 0;
+				while(sr.ReadLine() != null){
+					lineCount++;
+				}
+				fs.Close();
+				MessageBox.Show(lineCount.ToString());
+			}
+		}
 	}
 }
